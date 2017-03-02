098f02938a076c0c38c3d8f57fc61c020545ba9e
aquilax <aquilax@gmail.com>
2017-03-02 15:59:50 +0100

    Bumped version

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 const (
 	appName    = "hranoprovod-cli"
 	appUsage   = "Lifestyle tracker"
-	appVersion = "2.0.0"
+	appVersion = "2.0.1"
 	appAuthor  = "aquilax"
 	appEmail   = "aquilax@gmail.com"
 
