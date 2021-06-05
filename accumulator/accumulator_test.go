678d87d4e3407a8b7a4d296f9580b045c17c2fe4
aquilax <aquilax@gmail.com>
2021-06-05 09:37:26 +0200

    accumulator_test.go

--- accumulator/accumulator_test.go
+++ accumulator/accumulator_test.go
@@ -3,34 +3,34 @@ package accumulator
 import (
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/tj/assert"
 )
 
 func TestAccumulator(t *testing.T) {
-	Convey("Given the accumulator", t, func() {
+	t.Run("Given the accumulator", func(t *testing.T) {
 		acc := NewAccumulator()
-		Convey("When a positive element is added", func() {
+		t.Run("When a positive element is added", func(t *testing.T) {
 			acc.Add("test", 1.22)
-			Convey("It should go to the positive accumulator", func() {
-				So(acc["test"][Positive], ShouldEqual, 1.22)
+			t.Run("It should go to the positive accumulator", func(t *testing.T) {
+				assert.Equal(t, 1.22, acc["test"][Positive])
 			})
-			Convey("When a positive value is added to the same key", func() {
+			t.Run("When a positive value is added to the same key", func(t *testing.T) {
 				acc.Add("test", 2.33)
-				Convey("It is accumulated in the positive register", func() {
-					So(acc["test"][Positive], ShouldEqual, 3.55)
+				t.Run("It is accumulated in the positive register", func(t *testing.T) {
+					assert.Equal(t, 3.55, acc["test"][Positive])
 				})
 			})
-			Convey("When a negative value is added to the same key", func() {
+			t.Run("When a negative value is added to the same key", func(t *testing.T) {
 				acc.Add("test", -2.33)
-				Convey("It is accumulated in the positive register", func() {
-					So(acc["test"][Negative], ShouldEqual, -2.33)
-					So(acc["test"][Positive], ShouldEqual, 1.22)
+				t.Run("It is accumulated in the positive register", func(t *testing.T) {
+					assert.Equal(t, -2.33, acc["test"][Negative])
+					assert.Equal(t, 3.55, acc["test"][Positive])
 				})
 			})
-			Convey("When negative element is added", func() {
+			t.Run("When negative element is added", func(t *testing.T) {
 				acc.Add("test2", -1.32)
-				Convey("It should go to the negative accumulator", func() {
-					So(acc["test2"][Negative], ShouldEqual, -1.32)
+				t.Run("It should go to the negative accumulator", func(t *testing.T) {
+					assert.Equal(t, -1.32, acc["test2"][Negative])
 				})
 			})
 		})
